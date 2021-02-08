01d1c770f8bd349caad8fe55e2fea9775db06279
Wyjbuss <wyjbuss@gmail.com>
2021-02-07 21:09:56 -0600

    Note to self to implent use of axios in frontend to get rout from backend and do someting with it

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,10 +1,16 @@
 import React, { useState,useEffect } from 'react'
+import axios from 'axios'
+// use axios to get routs from backend
+// use youtube vid and docs https://www.youtube.com/watch?v=b6Oe2puTdMQ
 
 export default function Header(){
 
     const [favColor,setFavColor] = useState("red")
    
-   useEffect(()=>{console.log(`my fav color is ${favColor}`)},[])
+   useEffect(()=>{
+
+       console.log(`my fav color is ${favColor}`)
+    },[])
     
     return (
         <div>
